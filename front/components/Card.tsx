a3e1594bf3841a979aa93652ef441b430d2ec8d7
parkjuseoung <ehswmfql@gmail.com>
2021-09-05 20:17:56 +0900

    카드 스타일 수정

--- front/components/Card.tsx
+++ front/components/Card.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { FlatList } from 'react-native-gesture-handler';
 // import Lineargradient from 'react-native-linear-gradient';
 
-import styled from 'styled-components/native';
-import { COLORS, FONTS } from '../constants/theme';
+import styled, { css } from 'styled-components/native';
+import { COLORS, FONTS, FONTSIZES } from '../constants/theme';
 
 const DATA = [
   {
     id: 1,
     UserImage: '../assets/img_temp.png',
-    name: '이경량',
+    name: '박주성',
     time: '30분 초과',
     title: '몸에 좋은 나물 반찬',
     sub_title: '이런식으로 설명이 들어가면 좋을 것 같습니다.',
@@ -59,6 +59,10 @@ const renderItem = ({ item }) => {
       </CardContent>
 
       <CardFooter>
+        <FlagWrap>
+          <FlagItem goldLabel>#취나물</FlagItem>
+          <FlagItem>#조리시간 30분 미만</FlagItem>
+        </FlagWrap>
         <ButtonParent>
           <BookmarkButton source={require('../assets/Ico_bookmark.png')}></BookmarkButton>
         </ButtonParent>
@@ -127,13 +131,12 @@ const CardImageBackground = styled.ImageBackground`
   padding: 20px 16px;
 `
 const CookingTime = styled.Text`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: flex-end;
+  width: fit-content;
+  margin-left: auto;
   padding: 8px 12px;
   border-radius: 16px;
   background-position: 12px 50%;
-  background-size: 16px;
+  background-size: 16px 16px;
   background-image: url('../assets/ico_clock.png');
   background-repeat: no-repaet;
   background-color: rgba(255, 255, 255, .75);
@@ -143,20 +146,53 @@ const CardDescriptionWrap = styled.View`
   margin-top: auto;
 `
 const CardTitle = styled.Text`
-  font-size: ${FONTS.largestTitle};
+  font-size: ${FONTSIZES.largestTitle}px;
   font-weight: ${FONTS.fontBold};
   color: ${COLORS.white};
 `
 const CardSubTitle = styled.Text`
-  font-size: ${FONTS.midTitle};
+  font-size: ${FONTSIZES.midTitle}px;
   color: ${COLORS.white};
 `
 
 const CardFooter = styled.Text`
+  flex: 1;
+  display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
   align-items: center;
   min-height: 56px;
+`
+
+const FlagWrap = styled.View`
+  flex: 1;
+  display: inline-flex;
+  flex-flow: row wrap;
+  align-items: center;
+`
+
+interface StyledFlag {
+  goldLabel? : boolean,
+}
+
+const FlagItem = styled.Text<StyledFlag>`
+  margin-right: 8px;
+  padding: 8px 12px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${COLORS.gray200};
+  border-radius: 20px;
+  font-size: ${FONTSIZES.body3}px;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.white};
+  ${props =>
+    props.goldLabel &&
+    css`
+      border-width: 2px;
+      border-color: #ba7b3d;
+      color: #ba7b3d;
+      background-color: rgba(186, 123, 61, .06);
+    `}
 `
 
 export default Card;
