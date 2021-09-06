54e6a47068c7993b4a5142f5ecc3ad2b76c0d501
lee KR <benden4777@gmail.com>
2021-09-06 22:12:13 +0900

    top-탭바 커스텀 (인터넷에서 보고 따라한것으로 이해가 필요)

--- front/pages/Bookmark.tsx
+++ front/pages/Bookmark.tsx
@@ -3,6 +3,7 @@ import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import Recipe from './Recipe'
 import RecipeBook from './RecipeBook'
+import TabBar from '../components/Tabbar'
 
 
 const Tab = createMaterialTopTabNavigator();
@@ -10,7 +11,7 @@ const Tab = createMaterialTopTabNavigator();
 function Bookmark() {
 
 	return (
-		<Tab.Navigator>
+		<Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
 			<Tab.Screen name="레시피" component={Recipe} />
 			<Tab.Screen name="레시피북" component={RecipeBook} />
 		</Tab.Navigator>
