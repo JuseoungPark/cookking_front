c1e18d95ce51a94316594038eb05c7d4fcd10fd1
parkjuseoung <ehswmfql@gmail.com>
2021-09-08 23:59:12 +0900

    작성한 레시피 진행중

--- front/pages/RecipeWritten.tsx
+++ front/pages/RecipeWritten.tsx
@@ -24,8 +24,40 @@ const DATA = [
 	{
 		id: '5',
 		mainImage: '../assets/img_card_temp.png'
+	},
+	{
+		id: '6',
+		mainImage: '../assets/img_card_temp.png'
+	},
+	{
+		id: '7',
+		mainImage: '../assets/img_card_temp.png'
+	},
+	{
+		id: '8',
+		mainImage: '../assets/img_card_temp.png'
+	},
+	{
+		id: '9',
+		mainImage: '../assets/img_card_temp.png'
+	},
+	{
+		id: '10',
+		mainImage: '../assets/img_card_temp.png'
 	}
 ]
+
+const formatData = (DATA, numColumns) => {
+	const numberOfFullRows = Math.floor(DATA.length / numColumns);
+
+	let numberOfElmentsLastRow = DATA.length - (numberOfFullRows * numColumns);
+	while (numberOfElmentsLastRow !== numColumns) {
+		DATA.push({ key: `blank-$(numberOfElmentsLastRow)`, empty: true });
+		numberOfElmentsLastRow = numberOfElmentsLastRow + 1;
+	}
+
+	return DATA;
+}
 
 const RecipeWritten = () => {
 
@@ -38,6 +70,8 @@ const RecipeWritten = () => {
 		</GridItem>
 	);
 
+	const numColumns = 3;
+
 	return (
 		<FlatList
 			style={{
@@ -45,9 +79,10 @@ const RecipeWritten = () => {
 				flexDirection: 'row',
 				flexWrap: 'wrap',
 			}}
-			data={DATA}
+			data={formatData(DATA, numColumns)}
 			renderItem={renderItem}
 			keyExtractor={(item) => String(item.id)}
+			numColumns={numColumns}
 		/>
 	)
 }
@@ -57,21 +92,16 @@ const GridLayout = styled.FlatList`
 	flex-flow: row wrap;
 `
 
-interface Row {
-	row3? : Boolean
-}
-
-const GridItem = styled.View<Row>`
-	flex: none;
-	${props =>
-    props.row3 &&
-    css`
-			flex: 3;
-    `}
+const GridItem = styled.View`
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+	margin: 6px;
+	background-color: #bebebe;
 `
 const GridImage = styled.Image`
-	width: 124px;
-	height: 124px;
+	width: 112;
+	height: 112;
 `
 
 export default RecipeWritten;
